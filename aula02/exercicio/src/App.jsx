c733024c2b05d5ba852724777534e9125edf62f8
acramin <132026903+acramin@users.noreply.github.com>
2025-03-04 14:08:38 -0300

    Update App.jsx
    resolução do exercício

--- aula02/exercicio/src/App.jsx
+++ aula02/exercicio/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import n1 from "./assets/n1.jpeg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //console.log("Imagem carregada:", import.meta.env.VITE_IMG);
+  const container = () => {
+    return {
+      margin: "auto",
+      width: 1280,
+      backgroundColor: "#EEE",
+      padding: 12,
+      borderRadius: 8,
+      border: "1px solid black",
+      textAlign: "center",
+      color: "black",
+    };
+  };
+
+  const nomes = {
+    n1: "José da Silva",
+    n2: "Maria da Silva",
+    n3: "Jaqueline Mendes",
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={container()}>
+        <h2>Profissionais da Saúde</h2>
+        <div
+          style={{
+            margin: "auto",
+            width: "-webkit-fill-available",
+            backgroundColor: "#EEE",
+            padding: 12,
+            borderRadius: 8,
+            border: "1px solid black",
+            color: "black",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+          }}
+        >
+          <div className="card">
+            <img src={n1} />
+            <p>{nomes.n1}</p>
+          </div>
+          <div className="card">
+          <img src={import.meta.env.VITE_IMG} />
+            <p>{nomes.n2}</p>
+          </div>
+          <div className="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTx1_fMv7Jw00zeFzF2B657rTdgzkdEgkTBVOztH2Hwkpa5iArxKV1KGo&s" />
+            <p>{nomes.n3}</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
