6632aa70d285d24d03db798c302d43a11fa721bc
acramin <132026903+acramin@users.noreply.github.com>
2025-03-22 12:22:42 -0300

    iniciando exercício aula 05

---
+++ aula05/exercicio05/src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
+
+export default App
