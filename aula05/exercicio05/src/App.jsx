6d9a8bd8eefabce3f23da98fcc6f6666a1bd6b1f
acramin <132026903+acramin@users.noreply.github.com>
2025-03-22 12:38:35 -0300

    usando react-bootstrap

--- aula05/exercicio05/src/App.jsx
+++ aula05/exercicio05/src/App.jsx
@@ -1,12 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { Component } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 
 export class App extends Component {
   render() {
     return (
-      <div>App</div>
-    )
+      <div className="container mt-2">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <Card>
+              <Card.Header>Mega sena</Card.Header>
+              <Card.Body style={{ height: "6rem" }}>
+
+                <Button variant="light"> Gerar Jogo </Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
