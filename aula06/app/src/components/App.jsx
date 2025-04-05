b5ffb530904f1761ef66e8d96f6322faec838b62
acramin <132026903+acramin@users.noreply.github.com>
2025-04-05 11:48:19 -0300

    Update App.jsx
    import components

--- aula06/app/src/components/App.jsx
+++ aula06/app/src/components/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { Component } from "react";
+import Busca from "./Busca";
+import Lista from "./Lista";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="grid justify-content-center m-auto w-9 border-round border-1 border-400">
+        <div className="col-12">
+          <h1>Exibir uma lista de...</h1>
+        </div>
+        <div className="col-8">
+          <Busca />
+        </div>
+        <div className="col-8">
+          <Lista />
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App
+export default App;
