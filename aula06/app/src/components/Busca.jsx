8bfcfb7f1bcd9e193a06c0971bff14c0f089c5ab
acramin <132026903+acramin@users.noreply.github.com>
2025-04-05 12:42:22 -0300

    Update Busca.jsx
    add submit prevent

--- aula06/app/src/components/Busca.jsx
+++ aula06/app/src/components/Busca.jsx
@@ -10,25 +10,38 @@ export default class Busca extends Component {
   };
   onTermoAlterado = (event) => {
     console.log(event.target.value);
+    this.setState({ termoDeBusca: event.target.value });
   };
+
+  onFormSubmit = (event) => {
+    //não deixa o navegador submeter o form
+    event.preventDefault();
+    this.props.onBuscaRealizada(this.state.termoDeBusca);
+  };
+
   render() {
     return (
-      // empilhando os filhos
-      <div className="flex flex-column">
-        {/* ícone à esquerda, largura máxima */}
-        <span className="p-input-icon-left w-full">
-          <IconField iconPosition="left">
-            <InputIcon className="pi pi-search"> </InputIcon>
-            <InputText
-              //largura máxima
-              className="w-full"
-              onChange={this.onTermoAlterado}
-              placeholder={this.props.dica}
-            />
-          </IconField>
-        </span>
-        <Button label="OK" className="p-button-outlined mt-2" />
-      </div>
+      <form onFormSubmit={this.onFormSubmit}>
+        {
+          // empilhando os filhos
+        }
+        <div className="flex flex-column">
+          {/* ícone à esquerda, largura máxima */}
+          <span className="p-input-icon-left w-full">
+            <IconField iconPosition="left">
+              <InputIcon className="pi pi-search"> </InputIcon>
+              <InputText
+                value={this.state.termoDeBusca}
+                //largura máxima
+                className="w-full"
+                onChange={this.onTermoAlterado}
+                placeholder={this.props.dica}
+              />
+            </IconField>
+          </span>
+          <Button label="OK" className="p-button-outlined mt-2" />
+        </div>
+      </form>
     );
   }
 }
