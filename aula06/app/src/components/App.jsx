97c744d6a61b345c22b3e46a410d3321e58cd9a5
acramin <132026903+acramin@users.noreply.github.com>
2025-04-05 12:41:58 -0300

    Update App.jsx
    add submit func

--- aula06/app/src/components/App.jsx
+++ aula06/app/src/components/App.jsx
@@ -3,6 +3,10 @@ import Busca from "./Busca";
 import Lista from "./Lista";
 
 class App extends Component {
+  onBuscaRealizada = (termo) => {
+    console.log(termo);
+  };
+
   render() {
     return (
       <div className="grid justify-content-center m-auto w-9 border-round border-1 border-400">
