526a1a0331e0bda9486406ae5f4b680c1b703d97
acramin <132026903+acramin@users.noreply.github.com>
2025-04-05 10:54:19 -0300

    Update main.jsx
    add new imports

--- aula06/app/src/main.jsx
+++ aula06/app/src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App.jsx'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+import 'primereact/resources/themes/bootstrap4-light-purple/theme.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
