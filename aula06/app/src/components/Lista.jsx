99d030db8449b027dcf9fe4f9e8ca48ff7a8742a
acramin <132026903+acramin@users.noreply.github.com>
2025-04-05 11:48:41 -0300

    Create Lista.jsx

---
+++ aula06/app/src/components/Lista.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Item from "./Item";
+
+export class Lista extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Lista</h3>
+        <Item />
+      </div>
+    );
+  }
+}
+
+export default Lista;
